96a9a90d12db1f1f2639671a74f4382ea16d5156
Adriano <adriano_alves_2008@hotmail.com>
2023-11-30 11:45:30 -0300

    first commit

---
+++ src/main/java/com/spotifyapi/spotify/controller/AlbumController.java
@@ -0,0 +1,23 @@
+package com.spotifyapi.spotify.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spotifyapi.spotify.client.GoogleClient;
+
+@RestController
+@RequestMapping("/spotify/api")
+public class AlbumController {
+  private final GoogleClient googleClient;
+
+  public AlbumController(GoogleClient googleClient) {
+    this.googleClient = googleClient;
+  }
+
+  @GetMapping("/albums")
+  public ResponseEntity<String> helloWorld() {
+    return ResponseEntity.ok(googleClient.helloWolrd());
+  }
+}
