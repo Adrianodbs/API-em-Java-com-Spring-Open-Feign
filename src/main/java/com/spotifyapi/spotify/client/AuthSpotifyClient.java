9d54fa77ceda77979c978946c59b0fb197378d76
Adriano <adriano_alves_2008@hotmail.com>
2023-11-30 11:48:38 -0300

    Feat: create auth

---
+++ src/main/java/com/spotifyapi/spotify/client/AuthSpotifyClient.java
@@ -0,0 +1,8 @@
+package com.spotifyapi.spotify.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient(name = "AuthSpotifyClient", url = "https://accounts.spotify.com")
+public interface AuthSpotifyClient {
+
+}
